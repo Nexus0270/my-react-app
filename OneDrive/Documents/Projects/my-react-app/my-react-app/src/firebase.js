c075be67eafcc5b79fa7f0678b5780101e511269
Nexus0270 <muhammadhakeemramizal@gmail.com>
2025-09-01 16:38:34 +0800

    Landing Page Render

--- OneDrive/Documents/Projects/my-react-app/my-react-app/src/firebase.js
+++ OneDrive/Documents/Projects/my-react-app/my-react-app/src/firebase.js
@@ -10,7 +10,7 @@ const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "my-react-app-63f41.firebasestorage.app",
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
