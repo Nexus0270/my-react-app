17922183632c69f2caa05b535d5547e856b1ef56
Nexus0270 <muhammadhakeemramizal@gmail.com>
2025-09-01 15:56:53 +0800

    Firebase Git React

--- OneDrive/Documents/Projects/my-react-app/my-react-app/src/firebase.js
+++ OneDrive/Documents/Projects/my-react-app/my-react-app/src/firebase.js
@@ -7,13 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyDSrlflKS4mTpxQdyqzgmdbz96wcpxWUa4",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "my-react-app-63f41.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "my-react-app-63f41",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "my-react-app-63f41.firebasestorage.app",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "347351889216",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:347351889216:web:31d560129d39c5cb18b6a5",
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-4C89327RM0"
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Console logs to check if environment variables are loaded
